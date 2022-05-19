4b468582d3f8c8bc2ae97c6f03451825fa47b180
JoshBenPhilip <joshben4c@gmail.com>
2022-05-19 15:26:04 -0400

    added context

---
+++ src/context/ToDoListContext.js
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const ToDoListContext = createContext({});
+
+export const ToDoListContextProvider = ({ children}) => {
+    const { Provider } = ToDoListContext;
+    const [input, setInput] = useState('');
+    const [toDoList, setToDoList] = useState([]);
+
+    const value = {
+        input,
+        setInput,
+        toDoList,
+        setToDoList,
+    };
+    return <Provider value={value}>{children}</Provider>;
+};
